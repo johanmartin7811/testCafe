054c22cd97479a725400807787063ca44cd7fde5
Johan Martin <johanmartin@hotmail.se>
2025-05-05 09:36:35 +0200

    Added test for Expect no checkboxes to be visible

--- tests/checkBoxes.js
+++ tests/checkBoxes.js
@@ -15,12 +15,16 @@ test('Should expect 6 checkboxes are visible', async t => {
   await helpers.assertElementsCount(checkBoxes.all, 6, 'checkboxes');
 });
 
-test
-('Should expect all checkboxes are not checked', async t => {
+test('Expect no checkboxes to be visible', async t => {
+  await t.expect(page.main.visible).ok();
+  await helpers.hideElement('input[type="checkbox"]');
+  await helpers.assertAllHidden(checkBoxes.all, 'Checkbox');
+});
+
+test('Should expect no checkboxes are not checked', async t => {
   await t.expect(page.main.visible).ok();
   await helpers.assertAllNotChecked(checkBoxes.all, 'Checkbox');
 });
-
 
 test('Should assert two checked checkboxes', async t => {
   await t.expect(page.main.visible).ok();
@@ -31,11 +35,7 @@ test('Should assert two checked checkboxes', async t => {
   await t.expect(checkedCount).eql(2);
 });
 
-test('Expect no checkboxes to be visible', async t => {
-  await t.expect(page.main.visible).ok();
-  await helpers.hideElement('input[type="checkbox"]');
-  await helpers.assertAllHidden(checkBoxes.all, 'Checkbox');
-});
+
 
 
 
